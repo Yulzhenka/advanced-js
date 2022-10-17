3f27fea43e709ecd2f16a97258fa0e6a3f4283d0
Yulzhenka <acid_90@mail.ru>
2022-10-17 19:10:53 +0300

    dz4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict'; 
 
  
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+/*const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 function makeGETRequest(url) {
     const promise = new Promise((resolve,reject) => {
@@ -41,16 +41,24 @@ function makeGETRequest(url) {
 class GoodsList {
     constructor() {
         this.goods = [];
+        this.filteredGoods = [];
     }
     fetchGoods(cb) {
         makeGETRequest(`${API_URL}/catalogData.json`).then((goods) => {
             this.goods = JSON.parse(goods);
+            this.filteredGoods = JSON.parse(goods);
             cb(this.goods);
         });
     }
+
+    filterGoods(value){
+        const regexp = new RegExp(value,'i');
+        this.filteredGoods = this.goods.filter(good => regexp.test(good.product_name));
+        this.render();
+    }
     render() {
         let listHtml = '';
-        this.goods.forEach(good => {
+        this.filteredGoods.forEach(good => {
             const goodItem = new GoodsItem(good.product_name, good.price, good.img);
             listHtml += goodItem.render();
         });
@@ -65,6 +73,8 @@ class GoodsList {
         console.log(sum);
     }
 }
+const searchButton = document.querySelector('.search-button');
+const searchInput = document.querySelector('.goods-search');
 
 const list = new GoodsList();
 list.fetchGoods(() => {
@@ -72,7 +82,10 @@ list.fetchGoods(() => {
     list.getTotalPrice();
 });
 
-
+searchButton.addEventListener('click', (e) => {
+    const value = searchInput.value;
+    list.filterGoods(value);
+})
 //-----
 
 
@@ -89,7 +102,7 @@ class Box {
     }
     /*clearCart(){
         this.items = [] //очистить корзину
-    }*/
+    }
     addTotalPriceBox(){ //посчитать итог в корзине
         let sum = 0;
         for(let i=0; i<this.items.length; i++){
@@ -108,3 +121,31 @@ cart.show();
 cart.addTotalPriceBox();
 
 window.cart = cart;
+*/
+//ДЗ 4/1. Дан большой текст, в котором для оформления прямой речи используются одинарные
+//кавычки. Придумать шаблон, который заменяет одинарные кавычки на двойные.
+
+const bigText = 'I approached her, and said: \'My porter has a barrow\' Miss Bradley turned and looked at me. \'Oh — thank you. It is very kind of you.\' \'Well, I\'ll say good-bye now, and go and find my train. I expect the examiner\'ll come back and do you next.\'';
+console.log(bigText);
+const regexp = /'/g;
+console.log(regexp.test(bigText));
+console.log(bigText.replace(regexp,'"'));
+
+//ДЗ 4/2. Улучшить шаблон так, чтобы в конструкциях типа aren't одинарная кавычка не заменялась на
+//двойную.
+const regexpPro = / '/g;
+const regexpPro1 = /' /g; 
+const regexpPro2 = /\.'/g;
+const a = bigText.replace(regexpPro,' "');
+const b = a.replace(regexpPro1,'" ');
+const c = b.replace(regexpPro2,'"');
+console.log(c);
+
+//ДЗ 4/3.* Создать форму обратной связи с полями: Имя, Телефон, E-mail, текст, кнопка Отправить.
+//При нажатии на кнопку Отправить произвести валидацию полей следующим образом:
+//a. Имя содержит только буквы.
+//b. Телефон имеет вид +7(000)000-0000.
+//c. E-mail имеет вид mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru.
+//d. Текст произвольный.
+//e. Если одно из полей не прошло валидацию, необходимо выделить это поле красной рамкой
+//и сообщить пользователю об ошибке.
